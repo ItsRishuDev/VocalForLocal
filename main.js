8de134bbb3b1d5f3a480895a000b524323632b4d
Rishi Soni <rishisoni6071@gmail.com>
2020-10-19 13:52:33 +0530

    Vocal for Local

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 console.log('Indian Product - Vocal for local');
 
+
+// Product Details
+const products = document.querySelectorAll('.product-grid');
+products.forEach((product, id) => {
+    product.onclick = () => {
+        location.href = "productdetail.html";
+    }
+});
+
+
+// Switching of Login and Signup Form
+const switchToSignup = () => {
+    document.querySelector('#login_section').style.display = "none";
+    document.querySelector('#signup_section').style.display = "block";
+}
+const switchToLogin = () => {
+    document.querySelector('#signup_section').style.display = "none";
+    document.querySelector('#login_section').style.display = "block";
+}
+
+
+// Product Toggling
 let menuItems = document.getElementById('menu-item');
 menuItems.style.maxHeight = "0px";
 
@@ -13,19 +35,14 @@ function menutoggle() {
 }
 
 // For Product Galary
-
 let productImg = document.getElementById('productImg');
 let smallImg = document.getElementsByClassName('smallImg');
 
-smallImg[0].onclick = ()=>{
+smallImg[0].onclick = () => 
     productImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = ()=>{
+smallImg[1].onclick = () => 
     productImg.src = smallImg[1].src;
-}
-smallImg[2].onclick = ()=>{
+smallImg[2].onclick = () => 
     productImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = ()=>{
+smallImg[3].onclick = () => 
     productImg.src = smallImg[3].src;
-}
